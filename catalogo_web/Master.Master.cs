5fe04901a3132194b53de18012a9e021fb34ab8e
Nixkey <pokeloquendo102@gmail.com>
2024-02-05 22:38:47 -0300

    fix

--- catalogo_web/Master.Master.cs
+++ catalogo_web/Master.Master.cs
@@ -13,19 +13,19 @@ namespace catalogo_web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!(Page is Default || Page is Login || Page is Registro || Page is Error))
+            if (!(Seguridad.SesionActiva(Session["usuario"])))
             {
-                if (!(Seguridad.SesionActiva(Session["usuario"])))
+                if (!(Page is Default || Page is Login || Page is Registro || Page is Error))
                     Response.Redirect("Login.aspx", false);
+            }
+            else
+            {
+                Usuario usuario = (Usuario)Session["usuario"];
+                lblUser.Text = usuario.Email;
+                if (!(string.IsNullOrEmpty(usuario.UrlImagenPerfil)))
+                    imgAvatar.ImageUrl = $"~/Images/Profile/{usuario.UrlImagenPerfil}";
                 else
-                {
-                    Usuario usuario = (Usuario)Session["usuario"];
-                    lblUser.Text = usuario.Email;
-                    if (!(string.IsNullOrEmpty(usuario.UrlImagenPerfil)))
-                        imgAvatar.ImageUrl = $"~/Images/Profile/{usuario.UrlImagenPerfil}";
-                    else
-                        imgAvatar.ImageUrl = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
-                }
+                    imgAvatar.ImageUrl = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
             }
         }
         protected void btnSalir_Click(object sender, EventArgs e)
